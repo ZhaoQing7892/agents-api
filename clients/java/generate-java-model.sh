a827c1a0144bcec1555934415821223aede0e21d
zhaoqing <zq01297892@alibaba-inc.com>
2026-01-26 18:30:12 +0800

    Add Agents API Client For Java

---
+++ clients/java/generate-java-model.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Copyright 2020 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script generates the model classes from a released version of cert-manager CRDs
+# under src/main/java/io/cert/manager/models.
+
+DEFAULT_IMAGE_NAME=ghcr.io/kubernetes-client/java/crd-model-gen
+DEFAULT_IMAGE_TAG=v1.0.6
+IMAGE_NAME=${IMAGE_NAME:=$DEFAULT_IMAGE_NAME}
+IMAGE_TAG=${IMAGE_TAG:=$DEFAULT_IMAGE_TAG}
+
+
+# Check if the image exists locally, pull if not
+FULL_IMAGE="${IMAGE_NAME}:${IMAGE_TAG}"
+if [[ "$(docker images -q $FULL_IMAGE 2> /dev/null)" == "" ]]; then
+    echo "Image $FULL_IMAGE does not exist locally, pulling..."
+    docker pull "$FULL_IMAGE"
+else
+    echo "Image $FULL_IMAGE already exists locally."
+fi
+
+# a crdgen container is run in a way that:
+#   1. it has access to the docker daemon on the host so that it is able to create sibling container on the host
+#   2. it runs on the host network so that it is able to communicate with the KinD cluster it launched on the host
+docker run \
+  --rm \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$(pwd)":"$(pwd)" \
+  --network host \
+  ${IMAGE_NAME}:${IMAGE_TAG} \
+  /generate.sh \
+  -u https://raw.githubusercontent.com/openkruise/agents/master/config/crd/bases/agents.kruise.io_sandboxes.yaml \
+  -u https://raw.githubusercontent.com/openkruise/agents/master/config/crd/bases/agents.kruise.io_sandboxsets.yaml \
+  -n io.openkruise.agents.client \
+  -p io.openkruise.agents.client \
+  -o "$(pwd)"
