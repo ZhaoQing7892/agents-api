2350a0c0446e84bd663b5ed21837b33e8c921944
zhaoqing <zq01297892@alibaba-inc.com>
2026-04-14 17:37:31 +0800

    upgrade go sdk k8s version to v0.35

--- client/informers/externalversions/agents/v1alpha1/checkpoint.go
+++ client/informers/externalversions/agents/v1alpha1/checkpoint.go
@@ -57,7 +57,7 @@ func NewCheckpointInformer(client versioned.Interface, namespace string, resyncP
 // one. This reduces memory footprint and number of connections to the server.
 func NewFilteredCheckpointInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
 	return cache.NewSharedIndexInformer(
-		&cache.ListWatch{
+		cache.ToListWatcherWithWatchListSemantics(&cache.ListWatch{
 			ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
@@ -82,7 +82,7 @@ func NewFilteredCheckpointInformer(client versioned.Interface, namespace string,
 				}
 				return client.AgentsV1alpha1().Checkpoints(namespace).Watch(ctx, options)
 			},
-		},
+		}, client),
 		&agentsapiagentsv1alpha1.Checkpoint{},
 		resyncPeriod,
 		indexers,
--- client/informers/externalversions/agents/v1alpha1/sandboxclaim.go
+++ client/informers/externalversions/agents/v1alpha1/sandboxclaim.go
@@ -57,7 +57,7 @@ func NewSandboxClaimInformer(client versioned.Interface, namespace string, resyn
 // one. This reduces memory footprint and number of connections to the server.
 func NewFilteredSandboxClaimInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
 	return cache.NewSharedIndexInformer(
-		&cache.ListWatch{
+		cache.ToListWatcherWithWatchListSemantics(&cache.ListWatch{
 			ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
@@ -82,7 +82,7 @@ func NewFilteredSandboxClaimInformer(client versioned.Interface, namespace strin
 				}
 				return client.AgentsV1alpha1().SandboxClaims(namespace).Watch(ctx, options)
 			},
-		},
+		}, client),
 		&agentsapiagentsv1alpha1.SandboxClaim{},
 		resyncPeriod,
 		indexers,
